76a674d12e19103fbd9fdb3bc49fe48d46978ba0
sunburst <3520279278@qq.com>
2022-07-27 14:42:36 +0800

    feat(index.d.ts): 添加类型声明文件

--- femonitor-sdk/src/index.d.ts
+++ femonitor-sdk/src/index.d.ts
@@ -1,3 +1,20 @@
+export type IConfig = {
+  url: string;
+  //  设备Id
+  deviceId?: string;
+  //   用户id
+  userId?: string;
+  //   没有登录来确定用户就使用ip
+  ip?: string;
+  vue?: {
+    Vue: any;
+    router: any;
+  };
+  react?: {
+    React: any;
+    router: any;
+  };
+};
 export namespace monitor {
-  function init(options: any): any;
+  function init(options: IConfig): any;
 }
