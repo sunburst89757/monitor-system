0c859488e63dd5848c1b86ee845164f0df8c4351
sunburst <3520279278@qq.com>
2022-07-28 19:32:46 +0800

    feat(performance/xhr.js): xHR请求增加发送的数据和响应的数据

--- femonitor-sdk/src/performance/xhr.js
+++ femonitor-sdk/src/performance/xhr.js
@@ -1,43 +1,46 @@
-import { originalOpen, originalSend, originalProto } from '../utils/xhr'
-import { lazyReportCache } from '../utils/report'
+import { originalOpen, originalSend, originalProto } from "../utils/xhr";
+import { lazyReportCache } from "../utils/report";
 
 function overwriteOpenAndSend() {
-    originalProto.open = function newOpen(...args) {
-        this.url = args[1]
-        this.method = args[0]
-        originalOpen.apply(this, args)
-    }
+  originalProto.open = function newOpen(...args) {
+    this.url = args[1];
+    this.method = args[0];
+    originalOpen.apply(this, args);
+  };
 
-    originalProto.send = function newSend(...args) {
-        this.startTime = Date.now()
+  originalProto.send = function newSend(...args) {
+    this.startTime = Date.now();
+    this.sendData = args[0];
+    const onLoadend = (...args) => {
+      this.endTime = Date.now();
+      this.duration = this.endTime - this.startTime;
+      const responseData = args[0].currentTarget.response;
+      const { status, duration, startTime, endTime, url, method, sendData } =
+        this;
+      const reportData = {
+        status,
+        duration,
+        startTime,
+        endTime,
+        url,
+        sendData,
+        responseData,
+        method: (method || "GET").toUpperCase(),
+        success: status >= 200 && status < 300,
+        subType: "xhr",
+        type: "performance"
+      };
 
-        const onLoadend = () => {
-            this.endTime = Date.now()
-            this.duration = this.endTime - this.startTime
+      lazyReportCache(reportData);
 
-            const { status, duration, startTime, endTime, url, method } = this
-            const reportData = {
-                status,
-                duration,
-                startTime,
-                endTime,
-                url,
-                method: (method || 'GET').toUpperCase(),
-                success: status >= 200 && status < 300,
-                subType: 'xhr',
-                type: 'performance',
-            }
+      this.removeEventListener("loadend", onLoadend, true);
+    };
 
-            lazyReportCache(reportData)
-            
-            this.removeEventListener('loadend', onLoadend, true)
-        }
-
-        this.addEventListener('loadend', onLoadend, true)
-        originalSend.apply(this, args)
-    }
+    this.addEventListener("loadend", onLoadend, true);
+    originalSend.apply(this, args);
+  };
 }
 
 export default function xhr() {
-    overwriteOpenAndSend()
+  overwriteOpenAndSend();
 }
