5f47603bf69a7ed6a0b7c2f781c607f3ba15c570
sunburst <3520279278@qq.com>
2022-08-08 20:47:05 +0800

    feat(dataflowmap): 流量监测下流量数据绘制

---
+++ femonitor-admin/src/pages/Behavior/FlowData/componts/DataFlowMap.tsx
@@ -0,0 +1,57 @@
+import {
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+  QuestionCircleOutlined
+} from "@ant-design/icons";
+import { Tooltip } from "antd";
+import { useMemo } from "react";
+import { Map } from "../../../Dashboard/components/Map/Map";
+import style from "./dataFlowMap.module.scss";
+import { option1 } from "./config";
+type IProps = {
+  title: string;
+  promptMessage: string;
+  variation?: number;
+  time?: string;
+};
+export const DataFlowMap = ({ title, promptMessage, variation }: IProps) => {
+  const fontColor = useMemo(() => {
+    let color: string = "";
+    if (variation) {
+      if (variation > 0) {
+        color = "#00af56";
+      } else {
+        color = "#f85149";
+      }
+      return color;
+    }
+    return "#fff";
+  }, [variation]);
+  return (
+    <div>
+      <div className={style.mainHeader}>
+        <div className={style.leftBlock}>
+          {title}{" "}
+          <Tooltip title={promptMessage}>
+            <QuestionCircleOutlined />
+          </Tooltip>
+        </div>
+        {variation ? (
+          <div className={style.rightBlock} style={{ color: fontColor }}>
+            {variation + "%"}{" "}
+            {variation > 0 ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <div className={style.subHeader}>
+        <span>2022-08-08</span>
+        {variation ? <span>较一周前</span> : ""}
+      </div>
+      <div className={style.map}>
+        <Map option={option1} wAh={{ width: "23vw", height: "30vh" }}></Map>
+      </div>
+    </div>
+  );
+};
