abeebddb4d9d73920d94db7c1baf3d6e8874eb12
yomaii <i@lolly.cc>
2022-08-11 20:13:31 +0900

    add utils moudle

--- femonitor-backend/src/app.module.ts
+++ femonitor-backend/src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ErrorController } from './module/error/error.controller';
 import { ErrorModule } from './module/error/error.module';
 import { ReportModule } from './module/report/report.module';
 import { UserModule } from './module/user/user.module';
-import { ErrorController } from './module/error/error.controller';
+import { UtilsModule } from './module/utils/utils.module';
 
 const DBRootModule = MongooseModule.forRoot('mongodb://localhost/monitor')
 @Module({
-  imports: [DBRootModule, ErrorModule, ReportModule, UserModule],
+  imports: [DBRootModule, ErrorModule, ReportModule, UserModule, UtilsModule],
   controllers: [AppController, ErrorController],
   providers: [AppService],
 })
