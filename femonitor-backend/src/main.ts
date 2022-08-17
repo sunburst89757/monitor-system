f06b08367063f7ca3e2a3316a9d01c3c9dc38ed3
useshine <1154356171@qq.com>
2022-08-17 11:20:44 +0800

    add cors

--- femonitor-backend/src/main.ts
+++ femonitor-backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:true});
   app.use('/index/report', text());
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
